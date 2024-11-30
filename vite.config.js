85cc86003a7b6fe0ccfbd3398b2152368f72bc01
Jainam <127018175+JainamDosi@users.noreply.github.com>
2024-11-30 23:36:22 +0530

    package updated 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CHEMECA_IITB/', 
   build: {
     outDir: 'build', // Ensure the build output folder is named 'build'
   },
